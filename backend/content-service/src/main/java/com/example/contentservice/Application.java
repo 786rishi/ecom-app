5260ec8f6d935d974b12e661de9cbaffe1db1509
Rishi <rishi01@nagarro.com>
2026-03-22 17:09:27 +0530

    Initial commit

---
+++ backend/content-service/src/main/java/com/example/contentservice/Application.java
@@ -0,0 +1,23 @@
+
+package com.example.contentservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from " + appName;
+    }
+
+}
---
+++ backend/user-service/src/main/java/com/example/userservice/Application.java
@@ -0,0 +1,25 @@
+
+package com.example.userservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from " + appName;
+    }
+
+}

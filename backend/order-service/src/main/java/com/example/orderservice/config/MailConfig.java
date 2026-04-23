e8770583eceaf7d87bacf1b622cd2e12a32179d0
Rishi <rishi01@nagarro.com>
2026-04-23 23:39:42 +0530

    fix: email updated

--- backend/order-service/src/main/java/com/example/orderservice/config/MailConfig.java
+++ backend/order-service/src/main/java/com/example/orderservice/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.example.orderservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,14 +11,20 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${smtp.username}")
+    private String username;
+
+    @Value("${smtp.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
 
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
         sender.setHost("smtp.gmail.com");
         sender.setPort(587);
-        sender.setUsername("rishi.nihal.singh@gmail.com");
-        sender.setPassword("esmodbcttsuathlt");
+        sender.setUsername(username);
+        sender.setPassword(password);
 
         Properties props = sender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
--- backend/order-service/src/main/java/com/example/orderservice/service/EmailService.java
+++ backend/order-service/src/main/java/com/example/orderservice/service/EmailService.java
@@ -1,6 +1,11 @@
 package com.example.orderservice.service;
 
+import com.example.orderservice.client.ProductClient;
+import com.example.orderservice.dto.OrderItemResponse;
+import com.example.orderservice.dto.OrderResponse;
+import com.example.orderservice.dto.Product;
 import com.example.orderservice.entity.order.Order;
+import com.example.orderservice.entity.order.OrderItem;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +16,17 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
+    private final ProductClient productClient;
 
     @Async
     public void sendOrderConfirmationEmail(String to, Order order) throws MessagingException {
@@ -25,9 +35,47 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         Context context = new Context();
+
+        List<String> productIds = order.getItems().stream()
+                .map(OrderItem::getProductId)
+                .distinct() // avoid duplicates
+                .collect(Collectors.toList());
+
+        // 🔥 Step 2: Single API call
+        List<Product> products = productClient.getByIds(productIds);
+
+        // 🔥 Step 3: Convert to Map for fast lookup
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getId, p -> p));
+
+        // 🔥 Step 4: Map Orders → DTO
+            OrderResponse response = new OrderResponse();
+            response.setId(order.getId());
+            response.setUserId(order.getUserId());
+            response.setTotalAmount(order.getEffectiveAmount());
+            response.setDiscount(order.getDiscount());
+            response.setStatus(order.getStatus());
+            response.setCreatedAt(order.getCreatedAt());
+
+            List<OrderItemResponse> itemResponses = order.getItems().stream().map(item -> {
+
+                OrderItemResponse itemDTO = new OrderItemResponse();
+                itemDTO.setId(item.getId());
+                itemDTO.setQuantity(item.getQuantity());
+                itemDTO.setPrice(item.getPrice());
+
+                // 🔥 Attach full product
+                itemDTO.setProduct(productMap.get(item.getProductId()));
+
+                return itemDTO;
+
+            }).collect(Collectors.toList());
+
+            response.setItems(itemResponses);
+
         context.setVariable("orderId", order.getId());
-        context.setVariable("amount", order.getTotalAmount());
-        context.setVariable("items", order.getItems());
+        context.setVariable("amount", order.getEffectiveAmount());
+        context.setVariable("items", response.getItems());
 
         String html = templateEngine.process("order-confirmation", context);
 
